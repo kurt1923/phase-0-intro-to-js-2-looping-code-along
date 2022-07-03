6aea268a0de325b3852b42ab447b8e025f8a508b
Kurt Vermillion <kurtv0727@gmail.com>
2022-07-03 17:14:18 -0400

    commit tree b0afbbfbb55b3778e29c02ee4151f869226364ab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+function writeCards(cards, event){
+let messages =[]
+    for (let i = 0; i < cards.length; i++)
+    messages.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+    return messages
+}
+function countDown(int){
+    while (int >= 0)
+    console.log(int--)
+}
